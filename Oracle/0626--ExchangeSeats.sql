8fe01e805e71787b597de713c037c1335264ea56
Meggie Zhu <meggiezkf@gmail.com>
2021-04-15 15:42:08 +0800

    SQL 0626--ExchangeSeats

---
+++ Oracle/0626--ExchangeSeats.sql
@@ -0,0 +1,39 @@
+/*
+626. Exchange Seats
+Mary is a teacher in a middle school and she has a table seat storing students' names and their corresponding seat ids.
+
+The column id is continuous increment.
+
+Mary wants to change seats for the adjacent students.
+
+Can you write a SQL query to output the result for Mary?
+
+ 
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Abbot   |
+|    2    | Doris   |
+|    3    | Emerson |
+|    4    | Green   |
+|    5    | Jeames  |
++---------+---------+
+For the sample input, the output is:
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Doris   |
+|    2    | Abbot   |
+|    3    | Green   |
+|    4    | Emerson |
+|    5    | Jeames  |
++---------+---------+
+Note:
+If the number of students is odd, there is no need to change the last one's seat.
+*/
+
+SELECT ID,
+    DECODE(MOD(ID, 2), 1, LEAD(STUDENT, 1, STUDENT) OVER(ORDER BY ID), LAG(STUDENT, 1) OVER(ORDER BY ID)) AS STUDENT
+FROM SEAT
